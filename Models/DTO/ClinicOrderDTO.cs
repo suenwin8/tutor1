bafb996782fd2d8477c08594e79efc6f945fa2eb
chanswn <web.suen@gmail.com>
2023-03-16 17:32:55 +0800

    edit page - delete row function

--- Models/DTO/ClinicOrderDTO.cs
+++ Models/DTO/ClinicOrderDTO.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using tutor1.Models.Entity;
 
 namespace tutor1.Models.DTO
 {
     public class ClinicOrderDTO
     {
+        public int ClinicOrderId { get; set; }
         public string clinicOrder_seqid { get; set; }
         [DataType(DataType.Date)]
         public DateTime DateOfClinicOrder { get; set; }
